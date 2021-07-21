26634348604fdc29e5202cdfd8a90505b7b190b2
YuraMihailov <recon.97@mail.ru>
2021-07-22 01:32:50 +0300

    UPD: some work

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    #region Singleton
+    private static GameController _instance;
+
+    public static GameController Instance
+    {
+        get
+        {
+            try
+            {
+                if (_instance == null) _instance = GameObject.Find("Root").transform.Find("GameController").GetComponent<GameController>();
+            }
+            catch { }
+            return _instance;
+        }
+    }
+    #endregion
+
+
+    public GameObject cellPrefab;
+
+    public int level = 1;
+
+    public List<GameObject> cellsInScene = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
+    {
+        //cellPrefab = Resources.Load<GameObject>("Prefabs/cell");
+        level = 1;
+        cellsInScene = new List<GameObject>();
+    }
+
+    public void GenerateObjectsInCells()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/OnSpriteClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnSpriteClick : MonoBehaviour
+{
+    UnityEvent myEvent;
+
+    void Start()
+    {
+        if (myEvent == null)
+            myEvent = new UnityEvent();
+
+        myEvent.AddListener(Clicked);
+    }
+
+    void OnMouseDown()
+    {
+        myEvent.Invoke();
+    }
+
+    void Clicked()
+    {
+        Debug.Log("Clicked!");
+    }
+}
